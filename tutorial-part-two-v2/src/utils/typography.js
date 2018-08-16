4c53ee6cb6c3c6f0668b1dd92aa5100e64d1b27c
Glauber Castro <gfc714@gmail.com>
2018-08-16 10:28:22 -0300

    Add typography and new themes

--- tutorial-part-two-v2/src/utils/typography.js
+++ tutorial-part-two-v2/src/utils/typography.js
@@ -1,5 +1,8 @@
 import Typography from 'typography'
+// import bootstrapTheme from 'typography-theme-bootstrap'
+import funstonTheme from 'typography-theme-funston'
 
-const typography = new Typography({ baseFontSize: '18x' })
+// const typography = new Typography({ baseFontSize: '18x' })
+const typography = new Typography(funstonTheme)
 
 export default typography
